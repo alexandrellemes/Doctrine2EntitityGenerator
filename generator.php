278f9c6219ecca58fe625a647c1896a4bff037e1
Alexandre Pereira LLemes <alexandre.llemes@gmail.com>
2018-01-29 17:58:43 -0200

    Update generator.php

--- generator.php
+++ generator.php
@@ -13,15 +13,19 @@ $config->setMetadataCacheImpl(new \Doctrine\Common\Cache\ArrayCache);
 $config->setProxyDir(__DIR__ . '/Proxies');
 $config->setProxyNamespace('Proxies');
 
+//$connectionParams = array(
+//    'driver' => 'pdo_mysql',
+//    'host' => 'mysql',
+//    'port' => '3306',
+//    'user' => 'user.name',
+//    'password' => 'user.password',
+//    'dbname' => 'database.name',
+//    'charset' => 'utf8',
+//);
 
 $connectionParams = array(
-    'driver' => 'pdo_mysql',
-    'host' => 'mysql',
-    'port' => '3306',
-    'user' => 'user.name',
-    'password' => 'user.password',
-    'dbname' => 'database.name',
-    'charset' => 'utf8',
+    'driver' => 'pdo_sqlite',
+    'path' => '../database/database.sqlite',
 );
 
 $em = \Doctrine\ORM\EntityManager::create($connectionParams, $config);
@@ -32,17 +36,49 @@ $em->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping('enum',
 
 // fetch metadata
 $driver = new \Doctrine\ORM\Mapping\Driver\DatabaseDriver(
-                $em->getConnection()->getSchemaManager()
+    $em->getConnection()->getSchemaManager()
 );
-//$driver->setNamespace('Models\Entities');
+
+// Entities...
+$driver->setNamespace('Models\Entities\\');
 $em->getConfiguration()->setMetadataDriverImpl($driver);
 $cmf = new \Doctrine\ORM\Tools\DisconnectedClassMetadataFactory($em);
 $cmf->setEntityManager($em);
 $classes = $driver->getAllClassNames();
 $metadata = $cmf->getAllMetadata();
+
 $generator = new Doctrine\ORM\Tools\EntityGenerator();
 $generator->setUpdateEntityIfExists(true);
 $generator->setGenerateStubMethods(true);
 $generator->setGenerateAnnotations(true);
 $generator->generate($metadata, __DIR__ . '/Entities');
+
+print 'Done! - Entities... ' . PHP_EOL;
+
+// Repositories...
+// fetch metadata
+$driver = new \Doctrine\ORM\Mapping\Driver\DatabaseDriver(
+    $em->getConnection()->getSchemaManager()
+);
+$driver->setNamespace('Models\Repositories\\');
+$em->getConfiguration()->setMetadataDriverImpl($driver);
+$cmf = new \Doctrine\ORM\Tools\DisconnectedClassMetadataFactory($em);
+$cmf->setEntityManager($em);
+$classes = $driver->getAllClassNames();
+$metadata = $cmf->getAllMetadata();
+
+// Process destination directory
+$destPath = realpath(__DIR__ . '/Repositories');
+if (count($classes)) {
+    $numRepositories = 0;
+    $generator = new Doctrine\ORM\Tools\EntityRepositoryGenerator();
+    foreach ($classes as $class) {
+        echo 'Processing repository ' . $class . PHP_EOL;
+        $generator->writeEntityRepositoryClass( $class . 'Repository', $destPath);
+        $numRepositories++;
+    }
+    echo 'Number of repositories created at ' . $numRepositories . PHP_EOL;
+}
+
+
 print 'Done!' . PHP_EOL;
